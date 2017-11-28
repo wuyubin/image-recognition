4063398fef24bb55a04583ff97f40cebe9010318
wuyubin <18353616812@163.com>
2017-11-28 19:30:29 +0800

    Update lines.m

--- lines.m
+++ lines.m
@@ -1,12 +1,5 @@
 function [fl re]=lines(im_texto)
-% Divide text in lines
-% im_texto->input image; fl->first line; re->remain line
-% Example:
-% im_texto=imread('TEST_3.jpg');
-% [fl re]=lines(im_texto);
-% subplot(3,1,1);imshow(im_texto);title('INPUT IMAGE')
-% subplot(3,1,2);imshow(fl);title('FIRST LINE')
-% subplot(3,1,3);imshow(re);title('REMAIN LINES')
+
 im_texto=clip(im_texto);
 num_filas=size(im_texto,1);
 for s=1:num_filas
@@ -15,9 +8,7 @@ for s=1:num_filas
         rm=im_texto(s:end, :);% Remain line matrix
         fl = clip(nm);
         re=clip(rm);
-        %*-*-*Uncomment lines below to see the result*-*-*-*-
-        %         subplot(2,1,1);imshow(fl);
-        %         subplot(2,1,2);imshow(re);
+        
         break
     else
         fl=im_texto;%Only one line.
